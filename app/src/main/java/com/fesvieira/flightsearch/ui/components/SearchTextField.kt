c0d29ecdd1790273e330d052a167ab5ef7ab5388
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2023-07-19 16:26:00 -0300

    refactor top bar and text field code

---
+++ app/src/main/java/com/fesvieira/flightsearch/ui/components/SearchTextField.kt
@@ -0,0 +1,53 @@
+package com.fesvieira.flightsearch.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fesvieira.flightsearch.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTextField() {
+    TextField(
+        value = TextFieldValue(""),
+        onValueChange = {},
+        label = {
+            Text(text = "Enter departure airport")
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search),
+                contentDescription = null,
+                Modifier.size(24.dp)
+            )
+        },
+        shape = RoundedCornerShape(32.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color(0xffd5e3ff),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewSearchTextField() {
+    SearchTextField()
+}
